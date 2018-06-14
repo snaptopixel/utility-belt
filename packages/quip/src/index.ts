3e3e4ef11b888d439f91878698576c4e1c73b44b
Casey Corcoran <github@snaptopixel.com>
2018-06-14 14:49:32 -0400

    QUIP: Put everything in one file for easier packaging

--- packages/quip/src/index.ts
+++ packages/quip/src/index.ts
@@ -1,4 +1,3 @@
-import installPlugins from './plugins'
 import Vue, { CreateElement, VNode, VNodeData, VueConstructor, ComponentOptions } from 'vue'
 import { VueClass } from 'vue-class-component/lib/declarations'
 
@@ -7,7 +6,25 @@ export interface IComponents {
 }
 
 export interface IPlugins<T extends AllTagNames> {
-  // Populated via declaration merging
+  css (obj: any): IQuip<T>
+  css (...classes: string[]): IQuip<T>
+  style (name: keyof CSSStyleDeclaration, value: string): IQuip<T>
+  style (declaration: Partial<CSSStyleDeclaration>): IQuip<T>
+  on (event: string, callback: (...params: any[]) => void): IQuip<T>
+  on (events: {[event: string]: (...params: any[]) => void}): IQuip<T>
+  prop<K extends keyof AllTagProps[T]> (name: K, value: AllTagProps[T][K]): IQuip<T>
+  prop (props: AllPropTypes<T>): IQuip<T>
+  text (value: string): IQuip<T>
+  map<I> (items: I[], factory: (item: I) => void): IQuip<T>
+  callback (fn: (node: VNode) => void): IQuip<T>
+  switch (value: any): IQuip<T>
+  case (value: any, fn: () => void): IQuip<T>
+  default (fn: (value: any) => void): IQuip<T>
+  if (value: boolean | (() => boolean), fn: () => void): IQuip<T>
+  else (fn: () => void): IQuip<T>
+  attr (name: string, value: string): IQuip<T>
+  attr (obj: {[attr: string]: string}): IQuip<T>
+  data (data: VNodeData): IQuip<T>
 }
 
 export type PluginFn = (...args: any[]) => PluginCallback | void
@@ -52,8 +69,8 @@ export function registerPlugin<PluginName extends PluginNames> (name: PluginName
   Plugins[name] = fn
 }
 
-export function registerComponent<ComponentName extends keyof IComponents> (name: ComponentName, component: VueClass<Vue> | ComponentOptions<Vue>) {
-  Components[name] = component
+export function registerComponent (name: ComponentNames, component: VueClass<Vue> | ComponentOptions<Vue>) {
+  (Components as any)[name] = component
 }
 
 const tags: HtmlTagNames[] = Object.keys(HtmlTags).filter(key => isNaN(key as any)) as HtmlTagNames[]
@@ -144,4 +161,110 @@ export const QuipPlugin = {
   }
 }
 
-installPlugins()
+registerPlugin('css', (def: VNodeData, ...params: any[]) => {
+  def.class = def.class || {}
+  if (typeof params[0] === 'object') {
+    def.class = { ...def.class, ...params[0] }
+  } else if (typeof params[0] === 'string') {
+    def.class = {
+      ...def.class,
+      ...params.reduce((obj, className) => {
+        obj[className] = true
+        return obj
+      }, {})
+    }
+  }
+})
+
+registerPlugin('style', (def: VNodeData, ...params: any[]) => {
+  def.style = def.style || {}
+  if (typeof params[0] === 'object') {
+    def.style = { ...def.style, ...params[0] }
+  } else if (typeof params[0] === 'string') {
+    (def.style as any)[params[0]] = params[1]
+  }
+})
+
+registerPlugin('on', (def: VNodeData, ...params: any[]) => {
+  def.on = def.on || {}
+  if (typeof params[0] === 'object') {
+    def.on = { ...def.on, ...params[0] }
+  } else if (typeof params[0] === 'string') {
+    def.on[params[0]] = params[1]
+  }
+})
+
+registerPlugin('prop', (def: VNodeData, ...params: any[]) => {
+  def.props = def.props || {}
+  if (typeof params[0] === 'object') {
+    def.props = { ...def.props, ...params[0] }
+  } else if (typeof params[0] === 'string') {
+    def.props[params[0]] = params[1]
+  }
+})
+
+registerPlugin('text', (def: VNodeData, value: string) => {
+  return (vnode, createElement) => {
+    vnode.children.push(createElement('i', value).children[0])
+  }
+})
+
+registerPlugin('map', (def: VNodeData, items: any[], fn: (item: any) => void) => {
+  items.map(fn)
+})
+
+let switchValue: any
+let switchActive: boolean
+
+registerPlugin('switch', (def: VNodeData, value: any) => {
+  switchValue = value
+  switchActive = true
+  return () => {
+    switchActive = false
+  }
+})
+
+registerPlugin('case', (def: VNodeData, value: any, fn: () => void) => {
+  if (switchActive && switchValue === value) {
+    fn()
+    switchActive = false
+  }
+})
+
+registerPlugin('default', (def: VNodeData, fn: (value: any) => void) => {
+  if (switchActive) {
+    fn(switchValue)
+    switchActive = false
+  }
+})
+
+let doElse: boolean
+
+registerPlugin('if', (def: VNodeData, value: boolean | (() => boolean), fn: () => void) => {
+  if (typeof value === 'function') {
+    value = value()
+  }
+  doElse = !value
+  if (value) {
+    fn()
+  }
+})
+
+registerPlugin('else', (def: VNodeData, fn: () => void) => {
+  if (doElse) { fn() }
+})
+
+registerPlugin('attr', (def: VNodeData, ...params: any[]) => {
+  def.attrs = def.attrs || {}
+  if (typeof params[0] === 'object') {
+    def.attrs = { ...def.attrs, ...params[0] }
+  } else if (typeof params[0] === 'string') {
+    def.attrs[params[0]] = params[1]
+  }
+})
+
+registerPlugin('data', (def: VNodeData, data: VNodeData) => {
+  Object.keys(data).forEach((key: keyof VNodeData) => {
+    def[key] = data[key]
+  })
+})
